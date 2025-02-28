20fb39a783704bbac399929a06b9b530421b57ee
Ivan <v_cho@list.ru>
2025-02-28 19:50:28 +0300

    Password constraint check added

--- src/main/resources/db/migration/V2__insert-data.sql
+++ src/main/resources/db/migration/V2__insert-data.sql
@@ -6,3 +6,9 @@ VALUES('1', '1985-08-31', 'Kae', 'supervixen', '1111111111', 'yukawa@greenmail.i
 ('1', '1974-07-28', 'Hannah', 'bustyblonde', '2222222222', 'waddingham@greenmail.io', 'Waddingham', now(), ARRAY['ROLE_CLIENT']),
 ('1', '1995-06-13', 'Ella', 'gingerchick', '3333333333', 'hughes@greenmail.io', 'Hughes', now(), ARRAY['ROLE_ADMIN', 'ROLE_CLIENT'])
 ON CONFLICT DO NOTHING;
+
+SET @target_id = SELECT id FROM bankdemo.accounts WHERE phone='1111111111';
+INSERT INTO bankdemo.bills(is_active, balance, currency, owner, created_at)
+VALUES('1', '10.00', 'USD', @target_id, now()), ('1', '10.00', 'SEA', @target_id, now());
+SET @idx_array = ARRAY(SELECT id FROM bankdemo.bills WHERE owner = @target_id);
+UPDATE bankdemo.accounts SET bills = @idx_array WHERE phone='1111111111';
