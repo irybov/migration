d64042c8352fa889ea496587961c9d06215050ec
Ivan <v_cho@list.ru>
2025-03-17 13:36:09 +0300

    Transactional behave added

--- src/main/resources/db/migration/V2__insert-data.sql
+++ src/main/resources/db/migration/V2__insert-data.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 INSERT INTO bankdemo.accounts(is_active, birthday, name, password, phone, email, surname, created_at, roles)
 VALUES('1', '2001-01-01', 'Admin', 'superadmin', '0000000000', 'adminov@greenmail.io', 'Adminov', now(), ARRAY['ROLE_ADMIN'])
 ON CONFLICT DO NOTHING;
@@ -12,3 +14,5 @@ INSERT INTO bankdemo.bills(is_active, balance, currency, owner, created_at)
 VALUES('1', '10.00', 'USD', @target_id, now()), ('1', '10.00', 'SEA', @target_id, now());
 SET @idx_array = ARRAY(SELECT id FROM bankdemo.bills WHERE owner = @target_id);
 UPDATE bankdemo.accounts SET bills = @idx_array WHERE phone='1111111111';
+
+COMMIT;
